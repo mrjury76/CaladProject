1e40f39d5e628744161a0ee98f89281bc8b51a76
mrjury76 <mrjury76@gmail.com>
2024-03-25 10:04:02 -0700

    added random output Strings for the notification bar

--- app/src/main/java/com/example/project/NotificationReminder.java
+++ app/src/main/java/com/example/project/NotificationReminder.java
@@ -1,6 +1,5 @@
 package com.example.project;
 
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,23 +7,23 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
-
-import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import java.util.Random;
 
 public class NotificationReminder extends Service {
 
     public static final String CHANNEL_ID = "NOTIFICATION_REMINDER";
+
+    //Method that creates and calls the notification builder
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Create Notification channel for Oreo and above
         createNotificationChannel();
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,10, notificationIntent, PendingIntent.FLAG_MUTABLE);
 
         android.app.Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Calad - The Healthy Calendar App")
-                .setContentText("Don't forget to place your deliveries!")
+                .setContentText(notificationOutput())
                 .setSmallIcon(R.drawable.logo)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
@@ -33,12 +32,11 @@ public class NotificationReminder extends Service {
         startForeground(1, notification);
         return START_NOT_STICKY;
     }
+
+    //Method that checks for device api, if it's at least Oreo or higher, display the notification.
     private void createNotificationChannel() {
-        // Check if the Android version is Oreo or above
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
             NotificationManager manager = getSystemService(NotificationManager.class);
-
             NotificationChannel serviceChannel = new NotificationChannel(
                     CHANNEL_ID,
                     "Foreground Service Channel",
@@ -50,5 +48,25 @@ public class NotificationReminder extends Service {
 
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    //Method to randomize and choose a random notification string to output
+    public String notificationOutput() {
+        String output = "";
+        Random rand = new Random();
+        int randomString = rand.nextInt(5) + 1;
+
+        if (randomString == 1) {
+            output = "Don't forget to place your deliveries!";
+        } else if (randomString == 2) {
+            output = "Choose nutrient-rich options for a satisfying meal";
+        } else if (randomString == 3) {
+            output = "Check your calendar for any upcoming deliveries";
+        } else if (randomString == 4) {
+            output = "Time for a healthy snack! Choose wisely";
+        } else {
+            output = "Stay positive and keep moving forward!";
+        }
+        return output;
     }
 }
